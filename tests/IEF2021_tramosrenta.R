2b8da54d033f3df643a8d1d15f736270fc989411
il    iciuv <miguel.garcia.duch@gmail.com>
2024-03-05 19:20:15 +0100

    last

---
+++ tests/IEF2021_tramosrenta.R
@@ -0,0 +1,38 @@
+# Fichero principal 2020, select columns par150-caseros par36-renta
+library(magrittr)
+library(survey)
+library(data.table)
+rm(list = ls()) # clean enviroment to avoid ram bottlenecks
+
+# Parameters to set (Encuesta de referencia, periodo de estudio, unidad de estudi, columnas seleccionadaso)
+ref_survey <- "IEF" # either IEF or EFF
+sel_year <- 2021 # 2020 for EFF & 2021 for IEF
+ref_unit <- "IDENHOG" # Use either IDENPER for personal or IDENHOG for household levels
+selected_columns <- c("RENTAD", "RENTA_ALQ", "PATINMO")
+
+# Import choosen dataframe (cambiar string inicial según ruta de los datos)
+dt <- paste0("data/", ref_survey, "-", sel_year, "-new.gz") %>% fread()
+
+# Use lapply with .SDcols to specify columns and replace NA with 0
+dt[, (selected_columns) := lapply(.SD, function(x) ifelse(is.na(x), 0, x)), .SDcols = selected_columns]
+
+# Main data transformation TABLA[ filter_rows , select_columns  , group_by ]
+dt[TRAMO == "N", TRAMO := 8][, TRAMO := as.numeric(TRAMO)]
+
+dt <- dt[TIPODEC %in% c("T1", "T21") & !is.na(FACTORCAL),
+  .(
+    RENTAD = sum(RENTAD),
+    TRAMO = mean(TRAMO),
+    RENTA_ALQ = sum(RENTA_ALQ),
+    PAR150 = sum(PAR150),
+    PATINMO = sum(PATINMO),
+    FACTORCAL = mean(FACTORCAL)
+  ),
+  by = .(reference = get(ref_unit))
+]
+setnames(dt, "reference", as.character(ref_unit))
+dt_sv <- svydesign(ids = ~1, data = dt, weights = dt$FACTORCAL) # muestra con coeficientes de elevación
+
+renta_tramo <- svyby(~RENTAD, ~ as.factor(TRAMO), dt_sv, svymean)
+
+print(renta_tramo)
