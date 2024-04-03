b424486c2885f4cbaf7be9addef6c9d30fc29da1
il    iciuv <miguel.garcia.duch@gmail.com>
2024-04-03 18:50:40 +0200

    last

--- tests/EFF_tenencia_2011.R
+++ tests/EFF_tenencia_2011.R
@@ -1,18 +1,18 @@
-# Fichero principal 2020, select columns par150-caseros par36-renta
+# PArtial analysis
 library(magrittr)
 library(survey)
 library(data.table)
 rm(list = ls()) # clean enviroment to avoid ram bottlenecks
 
-# Parameters to set (Encuesta de referencia, periodo de estudio, unidad de estudi, columnas seleccionadaso)
-ref_survey <- "EFF" # either IEF or EFF
-sel_year <- 2011 # 2020 for EFF & 2021 for IEF
+# Parameters to set
+ref_survey <- "EFF"
+sel_year <- 2011
 
-# Import choosen dataframe (cambiar string inicial según ruta de los datos)
+# Import choosen dataframe
 dt <- paste0("data/", ref_survey, "-", sel_year, "-new.gz") %>% fread()
 
-## Prepare survey object from dt and set income cuts for quantiles dynamically
-dt_sv <- svydesign(ids = ~1, data = dt, weights = dt$facine3) # muestra con coeficientes de elevación
+## Prepare survey object
+dt_sv <- svydesign(ids = ~1, data = dt, weights = dt$facine3)
 
 # get tenure status
 tenencia <- svytable(~p2_1, dt_sv) %>% prop.table()
